524e2f31d58882e0d6ffb331067462c575c834e4
Janetsomehow <janetsomehow@gmail.com>
2021-07-24 00:40:02 +0100

    First dockerfile

---
+++ index.js
@@ -0,0 +1,13 @@
+//Load express module with `require` directive
+var express = require('express')
+var app = express()
+
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Welcome to SCA Cloud School Application!')
+})
+
+//Launch listening server on port 8080
+app.listen(8081, function () {
+  console.log('app listening on port 8081!')
+})
